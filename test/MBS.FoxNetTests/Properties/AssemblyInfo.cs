0bec53f9d73277d54e47c8a96fb8ff67e063b2a8
Joel Leach <jleach@mbs-intl.com>
2021-07-10 11:57:34 -0500

    Renamed MBS.FoxNetTests project to DotNet2Fox.Tests.

--- test/MBS.FoxNetTests/Properties/AssemblyInfo.cs
+++ test/MBS.FoxNetTests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MBS.FoxProTests")]
+[assembly: AssemblyTitle("DotNet2Fox.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MBS.FoxProTests")]
+[assembly: AssemblyProduct("DotNet2Fox.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
