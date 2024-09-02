3952ee1fe497c3a3d7998bae9901d89b88b51c36
Joel Leach <jleach@mbs-intl.com>
2024-09-02 09:49:42 -0500

    Updated version number to 1.1.

--- src/DotNet2Fox/Properties/AssemblyInfo.cs
+++ src/DotNet2Fox/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DotNet2Fox")]
-[assembly: AssemblyCopyright("Copyright © 2021 Joel Leach")]
+[assembly: AssemblyCopyright("Copyright © 2024 Joel Leach")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
